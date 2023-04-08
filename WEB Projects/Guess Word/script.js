36f06a70b57b92a564d388edfe9bae1467c7e777
linyaungoo <linyaungoo3199@gmail.com>
2023-04-08 17:53:56 +0630

    Guess number

---
+++ WEB Projects/Guess Word/script.js
@@ -0,0 +1,19 @@
+const input = document.getElementById("input_num");
+var random_num = Math.floor(Math.random()*10);
+
+const element = document.getElementById('check');
+    element.addEventListener("click",()=>{
+        const inputData = input.value;
+        console.log(inputData);
+        if(random_num < inputData){
+            document.getElementById("show_data").innerHTML = "Unknown number is less than ${inputData}";
+        }else if(random_num > inputData){
+            document.getElementById("show_data").innerHTML = "Unknown number is greater than ${inputData}";
+        }else if(random_num==inputData){
+            document.getElementById("show_data").innerHTML = " You Win!";
+        }console.log(random_num);
+    })
+
+
+
+
